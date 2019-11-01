25f64f27e7e8cd1e2fb1030d58a5587fae0c91ec
Zubeir Mohamed <zumo@solute.de>
2019-11-01 03:59:43 +0100

    initial commit :

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="todos">
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem v-bind:todo="todo"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+    name: "Todos",
+    components: {
+      TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
